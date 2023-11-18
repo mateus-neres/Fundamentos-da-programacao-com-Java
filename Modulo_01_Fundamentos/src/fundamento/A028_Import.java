32293d073f617b1fb29b89b4c8e8e6b978276351
mateus-neres <mateusneres03@gmail.com>
2023-11-18 09:38:24 -0300

    Update A028_Import

--- Modulo_01_Fundamentos/src/fundamento/A028_Import.java
+++ Modulo_01_Fundamentos/src/fundamento/A028_Import.java
@@ -1,9 +1,16 @@
 package fundamento;
 
+import java.util.Date;
+
 public class A028_Import {
 	public static void main(String[] args) {
 		
+		String s = "Bom dia!!";
+		System.out.println(s);
 		
+		
+		Date d = new Date();
+		System.out.println(d);
 		
 	}
 }
