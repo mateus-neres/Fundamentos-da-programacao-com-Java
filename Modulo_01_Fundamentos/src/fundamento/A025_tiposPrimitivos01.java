4d28ec3341a74ac700b6292f9532ef9d44742eb9
mateus-neres <mateusneres03@gmail.com>
2023-11-17 22:26:30 -0300

    Fundamentos
    Aula26 ,Tipos primitivo02

--- Modulo_01_Fundamentos/src/fundamento/A025_tiposPrimitivos01.java
+++ Modulo_01_Fundamentos/src/fundamento/A025_tiposPrimitivos01.java
@@ -1,44 +1,35 @@
 package fundamento;
 
 public class A025_tiposPrimitivos01 {
-	public static void main(String[] args) {
-		
-		/*
-		 * legenda: 
-		 *  "*" = Titulo de abertura
-		 *  "*->" = Subtitulo
-		 *  "->" = Topcos
-		 */
-		
+	public static void main(String[] args) {		
 		/*
 		 
 		 *Tipos primitivos:
 		
-			*-> Em java temos 8 tipos primitovos.
-			*-> 06 desses tipos sãonumericos, sendo eles, 4 para números inteiros e 2 para números reais reais.
+			Em java temos 8 tipos primitovos, 06 desses tipos sãonumericos, sendo eles, 4 para números inteiros e 2 para números reais reais.
 		
-		 	*-> Tipos primitivos que amazenam números inteiros:
+		 	Tipos primitivos que amazenam números inteiros:
 		
 				-> byte: O byte, corresponde a exatamente um byte de espaço, ou seja, 8 bits.
 				-> short: O short, corresponde a 2 bytes, ou 16 bits.
 				-> int: O int, conrresponde a 4 bytes ou 32 bits.
 				-> long: O long, corresponde a 8 bytes ou 64 bits.
 		
-		 	*-> Tipos primitivos que armazenam números reais:
+		 	Tipos primitivos que armazenam números reais:
 			
 				-> float: O float, corresponde a 4 bytes ou 32 bits.
 				-> double: O double, corresponde a 8 bytes ou 64 bits.
 		
-		 	*Notação -> um bit corresponde a um espaço na memoria, enquanto um byte, tem valor igual a 8 unidades de bits de armazenamento, pensando nisso, entendemos que
+		 	Notação -> um bit corresponde a um espaço na memoria, enquanto um byte, tem valor igual a 8 unidades de bits de armazenamento, pensando nisso, entendemos que
 		 	uma valor que oculpe mais que a capacidade de aramazenamento de um tipo, não poderá ser atribuido a ele.
 		 	Ex: double a = 3 -> 3 é um valor inteiro e ocupa um espaço de memoria menor, logo o tipo primitivo pode ser usado;
-		 	    int b = 3.5 -> 3.5 é um valor que excede o limite de armazenamento, então o tipo primitivo "int" não poderá ser usado aqui. 
+		 	int b = 3.5 -> 3.5 é um valor que excede o limite de armazenamento, então o tipo primitivo "int" não poderá ser usado aqui. 
 		 	    
-		 	   Resumindo, um valor "int" pode ser guardado em um "double", no entanto um "double", não pode ser guardado em um "int"
+		 	Resumindo, um valor "int" pode ser guardado em um "double", no entanto um "double", não pode ser guardado em um "int"
 		
-		 	*A diferença basica entre os tipos numericos são seus comprimentos.
+		 	A diferença basica entre os tipos numericos são seus comprimentos.
 		 	
-		 	*-> Tipos primitivos não numericos:
+		 	Tipos primitivos não numericos:
 		 		-> char: O char, armazena caracteres alfanumericos, e é representado por aspas ' ' simples, contendo os caracter dentro das aspas
 		 		-> boolean: O boolean, representa os valores logicos, "true" ou "false", verdadeiro ou falso respectiviamente.	
 		 		
