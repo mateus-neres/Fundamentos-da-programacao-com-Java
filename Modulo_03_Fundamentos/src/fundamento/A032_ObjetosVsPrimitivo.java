1833f8e7d5437745cc87b7bfdf141ee8dcca6849
mateus-neres <mateusneres03@gmail.com>
2023-11-19 00:52:59 -0300

    A033_Wrapper

--- Modulo_03_Fundamentos/src/fundamento/A032_ObjetosVsPrimitivo.java
+++ Modulo_03_Fundamentos/src/fundamento/A032_ObjetosVsPrimitivo.java
@@ -1,5 +1,16 @@
 package fundamento;
 
 public class A032_ObjetosVsPrimitivo {
-
+	
+	public static void main(String[] args) {
+		
+		String s = "texto";
+		s.toUpperCase();
+		
+		// Wrappers são a versão objetos dos tipos primitivos
+		int a = 123;
+		System.out.println(a);
+		
+	}
+	
 }
