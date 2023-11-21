9be6b51526a6bf016582d28c34f5e230624e1fe7
mateus-neres <mateusneres03@gmail.com>
2023-11-20 23:05:40 -0300

    Update A34_ConversoaDeTiposPrimitivos

--- Modulo_03_Fundamentos/src/fundamento/A34_ConversaoDeTiposPrimitivos01.java
+++ Modulo_03_Fundamentos/src/fundamento/A34_ConversaoDeTiposPrimitivos01.java
@@ -11,6 +11,20 @@ public class A34_ConversaoDeTiposPrimitivos01 {
 		 *Porém o inverso é realizado de forma explicita.
 		 *Ex: Byte <<-- Short <<-- Int <<-- Long
 		 *
+		 *
+		 *Tome como base a o exemplo abaixo:
+		 *
+		 *Int -->> Float
+		 *3 -->> 3.0
+		 *Não há perdas pois os número inteiro 3, apenas ganhou uma casa decimal, isso foi uma conversão implicita.
+		 * 
+		 *Float -->> Int
+		 *3.2 -->> 3
+		 *Há uma perda, pois o número com ponto flutuante perdeu sua casa decimal, isso foi uma conversão implicita.
+		 *
+		 *Resumindo, o java converte automatico se você sair de um tipo primitivo menor para um tipo primitivo maio, caso contrario tem que haver uma conversão explicita.
+		 *
+		 *Obs: O java não analisa valores e sim o tipo atribuido.
 		 */
 		
 	}
