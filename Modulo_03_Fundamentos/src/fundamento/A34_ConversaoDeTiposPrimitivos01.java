40ad848039b8c7641c362cd08edc090de659d87d
mateus-neres <mateusneres03@gmail.com>
2023-11-20 22:55:23 -0300

    A34_ConversaoDeTiposPrimitivos01.

---
+++ Modulo_03_Fundamentos/src/fundamento/A34_ConversaoDeTiposPrimitivos01.java
@@ -0,0 +1,18 @@
+package fundamento;
+
+public class A34_ConversaoDeTiposPrimitivos01 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * O java realiza conversão inplicita de Tipos Primitivos menores ara os maiores.
+		 * Ex: Byte -- >> Short -->> Int -->> Long 
+		 * 
+		 *Porém o inverso é realizado de forma explicita.
+		 *Ex: Byte <<-- Short <<-- Int <<-- Long
+		 *
+		 */
+		
+	}
+	
+}
