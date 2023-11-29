6da67d9a9a578cfb6ba9dbdf727dcd2e21f91b9c
mateus-neres <mateusneres03@gmail.com>
2023-11-29 00:41:08 -0300

    .

---
+++ Modulo_03_Fundamentos/src/fundamento/A041_Operadores.java
@@ -0,0 +1,25 @@
+package fundamento;
+
+public class A041_Operadores {
+
+	public static void main(String[] args) {
+		
+		/* Operadore unário
+		 *  a++
+		 */
+		
+		/* Operadore binário
+		 *  Aritmeticos
+		 *  a + b
+		 */
+		
+		/* Operadore Ternário
+		 *  a ? a ? a
+		 */
+		
+		
+		
+		
+	}
+	
+}
