0c9b08025c3dacdd4d965c048ec6f4c0061dd391
mateus-neres <mateusneres03@gmail.com>
2023-12-21 23:18:34 -0300

    .

--- Java 2022/src/operadores/A048_DesafioLogicos.java
+++ Java 2022/src/operadores/A048_DesafioLogicos.java
@@ -1,4 +1,16 @@
 package operadores;
 
 public class A048_DesafioLogicos {
+    public static void main(String[] args) {
+        boolean primeiroTrabaho = false;
+        boolean segundoTrabalho = false;
+
+        if (primeiroTrabaho == true && segundoTrabalho == true){
+            System.out.println("Compra TV de 50 e toma sorvete");
+        }else if (primeiroTrabaho == true || segundoTrabalho == true) {
+            System.out.println("Compra TV de 35 e toma sorvete");
+        }else {
+            System.out.println("Não sairam para tomar sorvete");
+        }
+    }
 }
---
+++ Java 2022/src/operadores/A050_Relacionais.java
@@ -0,0 +1,4 @@
+package operadores;
+
+public class A050_Relacionais {
+}
