dd8de25fb8ac3d6c62247f29ffcb497ce31157a5
mateus-neres <mateusneres03@gmail.com>
2023-12-21 23:34:13 -0300

    Update A050_Relacionais

--- Java 2022/src/operadores/A050_Relacionais.java
+++ Java 2022/src/operadores/A050_Relacionais.java
@@ -1,4 +1,10 @@
 package operadores;
 
 public class A050_Relacionais {
+    public static void main(String[] args) {
+
+        int a = 97;
+        int b = 'a';
+        System.out.println(a == b);
+    }
 }
